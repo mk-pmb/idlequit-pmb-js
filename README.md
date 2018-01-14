f2de6ad38dabc42d37c85dcf186d5eba79f9766c
mk-pmb <mk-pmb@users.noreply.github.com>
2018-01-14 11:47:17 +0100

    API draft

---
+++ README.md
@@ -0,0 +1,37 @@
+﻿
+<!--#echo json="package.json" key="name" underline="=" -->
+idlequit-pmb
+=============
+<!--/#echo -->
+
+<!--#echo json="package.json" key="description" -->
+Inspired by autoquit but with a more explicit API.
+<!--/#echo -->
+
+
+Usage
+-----
+
+Maybe sth. like this? [api-idea.js](api-idea.js)
+
+
+<!--#toc stop="scan" -->
+
+
+
+Known issues
+------------
+
+* Needs more/better tests and docs.
+
+
+
+
+&nbsp;
+
+
+License
+-------
+<!--#echo json="package.json" key=".license" -->
+ISC
+<!--/#echo -->
---
+++ api-idea.js
@@ -0,0 +1,33 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+var idleQuit = require('idlequit-pmb'),
+  idleWatcher = idleQuit({ idleLimitMinutes: 10,
+    checkIntvMinutes: 5,    // default: idleLimitMinutes / 2
+    hardkillSec: 10,        // default: false
+    peerIdleMinutes: 5,     // default: false
+    }),
+  netLib = require('net'),
+  streamServer = netLib.createServer(),
+  peerSocket = netLib.connect({ host: 'localhost', port: 1234 });
+
+idleWatcher.watchNetSocket(peerSocket);
+// no 2nd arg = use this watcher's default options.
+// returns function unwatch() {…} which also carries
+// the current options as its properties.
+
+idleWatcher.watchNetServer(streamServer, {
+  closeOnIdle: true,        // default
+  peerIdleMinutes: false,   // any connection
+});
+
+idleWatcher.on('beforeIdle', function (ev) {
+  if (streamServer.waiting4peer) {
+    ev.notIdle();   // once-per-event proxy to idleWatcher.buzzer();
+  }
+});
+
+streamServer.on('almostDone', function () {
+  idleWatcher.extendUntil(Date.now() + 5e3);
+});
